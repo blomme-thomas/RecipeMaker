4e41e0a1f0853539b6c42747fbe3516320341202
thomas-blomme <blommethomas59@gmail.com>
2024-08-07 20:46:47 +0200

    Connexion vers la base distante postgres

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,27 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+      ssl: {
+        rejectUnauthorized: false,
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
